12fe7e99ae29f5a850da7625d5741f935ede44e3
Abriel14 <mathieu.vallee1998@gmail.com>
2020-11-26 20:41:11 +0900

    this is nothing

--- test_if_promising.py
+++ test_if_promising.py
@@ -15,11 +15,11 @@ l=0
 
 for K_bytes in results:
     K_bin= json.loads(K_bytes)
-    K_sp = sc.PureSimplicialComplex(K_bin)
+    K_sp = sc.PureSimplicialComplex()
     if K_sp.is_Z2_homology_sphere():
         if K_bin not in K_result:
             K_result.append(K_bin)
-            print(K_bin)
+
     # list_2_pow = [1]
     # for k in range(8):
     #     list_2_pow.append(list_2_pow[-1]*2)
@@ -34,4 +34,4 @@ for K_bytes in results:
     # Chain, Ind = Partial_Enum_PLS_IDCM.Chain_cpx(K, 6)
     # if Partial_Enum_PLS_IDCM.is_Homology_Sphere(Chain, Ind) and K not in K_result:
     #     K_result.append(K)
-print(len(K_result))
+print(K_result)
