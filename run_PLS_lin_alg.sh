440c75539e460557190fa71070cf6391ea4bc90a
vallee <mathieu.vallee1998@gmail.com>
2022-01-14 18:15:14 +0100

    Running 9-13

--- run_PLS_lin_alg.sh
+++ run_PLS_lin_alg.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=PLS_8_12
+#SBATCH --job-name=PLS_9_13_1
 #SBATCH --gres=gpu:1
 #SBATCH --qos=qos_gpu-t4
 #SBATCH --cpus-per-task=5
-#SBATCH --output=./PLS_8_12.out
-#SBATCH --error=./PLS_8_12.err
+#SBATCH --output=./PLS_9_13_1.out
+#SBATCH --error=./PLS_9_13_1.err
 #SBATCH --time=100:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=5
