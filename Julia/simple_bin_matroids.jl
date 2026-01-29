a3ccb5cf00416c05f1fbc075bf80b32ff9435552
Mathieu Vallée <mathieu.vallee1998@gmail.com>
2026-01-29 18:19:11 +0100

    First test for pic 5

--- Julia/simple_bin_matroids.jl
+++ Julia/simple_bin_matroids.jl
@@ -1,19 +1,19 @@
 using Oscar
+using IterTools
+using ProgressBars
+using LinearAlgebra
 
 function all_nonzero_binary_vectors(n)
-    k = 2^n - 1
+    k = (2^n) - 1
     mat = Matrix{Int}(undef, k, n)
-    row = 1
     for i in 1:k
         v = digits(i, base=2, pad=n)
-        mat[row, :] = reverse(v)
-        row += 1
+        mat[i,:]= v
     end
     return mat
 end
 
-global A = matrix(GF(2), all_nonzero_binary_vectors(5))
-global M0 = matroid_from_matrix_rows(A)
+
 
 # à corriger car problème de rang...
 
@@ -21,10 +21,13 @@ function find_lower_dim_matroids(list_bin_mat)
     local S_rep = Set{Matroid}()
     for M in list_bin_mat
         for v in matroid_groundset(M)
-            if [v] in cocircuits(M) # si v est un loop, on skip
+            if v in coloops(M)
                 continue
             end
             M1 = deletion(M,v)
+            if rank(M)!=rank(M1)
+                continue
+            end
             is_isom=false
             for M2 in S_rep
                 if is_isomorphic(M1,M2)
@@ -33,8 +36,8 @@ function find_lower_dim_matroids(list_bin_mat)
                 end
             end
             if is_isom==false
-                str = revlex_basis_encoding(M1)
-                push!(S_rep,matroid_from_revlex_basis_encoding(str,rank(M1),length(M1)))
+                
+                push!(S_rep,M1)
             end
         end
     end
@@ -42,10 +45,12 @@ function find_lower_dim_matroids(list_bin_mat)
 end
 
 global m=31
+global A = matrix(GF(2), all_nonzero_binary_vectors(5))
+global M0 = matroid_from_matrix_rows(A)
 global S = Set{Matroid}()
 push!(S,M0)
 while m > 5
-    io=open("bin_mat_" * string(m) * "_" * string(m-4),"w")
+    io=open("bin_mat_better_" * string(m) * "_" * string(m-5),"w")
     print("number of elements ",m," number of matroids ",length(S),"\n")
     for M in S
         println(io,cobases(M))
@@ -54,6 +59,31 @@ while m > 5
     global S = find_lower_dim_matroids(S)
     global m-=1
 end
+
+# for i=2:m
+#     S=Set{Matroid}()
+#     for J in ProgressBar(IterTools.subsets(axes(A,1),i+5))
+#         M1=matroid_from_matrix_rows(A[J,:])
+#         is_isom=false
+#         for M2 in S
+#             if is_isomorphic(M1,M2)
+#                 is_isom=true
+#                 break
+#             end
+#         end
+#         if !is_isom
+#             push!(S,M1)
+#         end
+#     end
+#     io=open("bin_mat_" * string(i+5) * "_" * string(i),"w")
+#     print("number of elements ",m," number of matroids ",length(S),"\n")
+#     for M in S
+#         println(io,cobases(M))
+#     end
+#     close(io)
+# end
+
+
 
 
 
